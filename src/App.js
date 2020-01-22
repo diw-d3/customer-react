16017a69b9c678aec88b48b97fb97f6694186b18
Matthieu Mota <matthieumota@gmail.com>
2020-01-22 10:56:04 +0100

    Customer manager

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      customers: [
+        { id: 1, name: 'Matthieu' },
+        { id: 2, name: 'Toto' }
+      ],
+      newCustomer: ''
+    };
+  }
+
+  handleChange = (event) => {
+    // On récupère la valeur saisie dans le formulaire d'ajout
+    console.log(event.target.value);
+    this.setState({ newCustomer: event.target.value });
+  }
+
+  /**
+   * Traitement du formulaire
+   */
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('FORMmmmm : ' + this.state.newCustomer);
+
+    // Ajouter le nouveau client dans le state...
+    let customers = this.state.customers.slice(); // slice() permet de faire une copie du tableau
+    customers.push({ id: customers.length + 1, name: this.state.newCustomer });
+    // On remplace le tableau customers
+    this.setState({ customers: customers });
+  }
+
+  handleDelete = (event, index) => {
+    // Supprimer le client cliqué du tableau
+    let customers = this.state.customers.slice();
+
+    // Je dois trouver dans le tableau, le client qui a l'index concerné
+    customers.splice(index, 1);
+
+    this.setState({ customers: customers });
+  }
+
+  render() {
+    return (
+      <div>
+        <ul>
+          {this.state.customers.map((customer, index) =>
+            <li key={customer.id}>
+              {customer.name} <button onClick={(e) => this.handleDelete(e, index)}>X</button>
+            </li>
+          )}
+        </ul>
+
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" value={this.state.newCustomer} onChange={this.handleChange} />
+          <button>Ajouter</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
